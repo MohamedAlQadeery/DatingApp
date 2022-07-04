d397d2cf32216fe617d2ef805372f69c992c0da3
MohamedAlQadeery <moh9amad1@gmail.com>
2022-07-05 02:13:12 +0300

    Added Angular Front End And Consumed GetUsers Api/installed ngx-bootstrap

--- API/Startup.cs
+++ API/Startup.cs
@@ -35,6 +35,7 @@ namespace API
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
@@ -55,6 +56,10 @@ namespace API
 
             app.UseRouting();
 
+            app.UseCors(policy =>
+            {
+                policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+            });
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
---
+++ client/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  constructor(private _http : HttpClient){}
+
+  title = 'The Dating App';
+  users : any;
+
+
+  ngOnInit(): void {
+   this.GetUsers();
+  }
+
+  private GetUsers() {
+    this._http.get('https://localhost:5001/api/users').subscribe(response => {
+      this.users = response;
+    }, error => {
+      console.log(error);
+    });
+  }
+}
