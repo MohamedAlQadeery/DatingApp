21c1cf1c4ece84fec3dca1c20368ba49b8c36a8e
MohamedAlQadeery <moh9amad1@gmail.com>
2022-07-07 17:58:57 +0300

    Created IUserRepository And UserRepository

---
+++ API/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using API.Entities;
+
+namespace API.Interfaces.Repositories
+{
+    public interface IUserRepository
+    {
+        //Mark this entity thats its modifed
+        void Update(AppUser user);
+        // Returns true if database is updated with the changes
+        Task<bool> SaveAllAsync();
+        Task<AppUser> GetUserByIdAsync(int id);
+        Task<AppUser> GetUserByUsernameAsync(string username);
+        Task<IEnumerable<AppUser>> GetUsers();
+    }
+}
