bee0aaf0259c33961c39262e1a13945af94e5e96
MohamedAlQadeery <moh9amad1@gmail.com>
2022-07-08 19:11:17 +0300

    Memeber Edit Component/Prevent un save changes guard

---
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -0,0 +1,52 @@
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { take } from 'rxjs/operators';
+import { IMember } from 'src/app/_models/member';
+import { IUser } from 'src/app/_models/user';
+import { AccountService } from 'src/app/_services/account.service';
+import { MembersService } from 'src/app/_services/members.service';
+
+@Component({
+  selector: 'app-member-edit',
+  templateUrl: './member-edit.component.html',
+  styleUrls: ['./member-edit.component.css'],
+})
+export class MemberEditComponent implements OnInit {
+  @ViewChild('editForm') editForm: NgForm;
+  member: IMember;
+  currentUser: IUser;
+
+  @HostListener('window:beforeunload', ['$event']) UnloadNotification(
+    $event: any
+  ) {
+    if (this.editForm.dirty) {
+      $event.returnValue = true;
+    }
+  }
+  constructor(
+    private _accountService: AccountService,
+    private _memberService: MembersService,
+    private _tostr: ToastrService
+  ) {
+    this._accountService.currentUser$.pipe(take(1)).subscribe((user) => {
+      this.currentUser = user;
+    });
+  }
+
+  ngOnInit(): void {
+    this.LoadMemberInfo();
+  }
+
+  private LoadMemberInfo() {
+    this._memberService
+      .GetMember(this.currentUser.username)
+      .subscribe((member) => (this.member = member));
+  }
+
+  UpdateMember() {
+    console.log(this.member);
+    this._tostr.success('Profile Updated Succesfully!');
+    this.editForm.reset(this.member);
+  }
+}
